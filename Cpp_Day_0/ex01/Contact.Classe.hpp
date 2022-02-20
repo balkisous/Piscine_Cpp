ae3c9304be7bc528e392df4a7d18ad5ff9d1899e
balkisous <balkisous@gmail.com>
2022-02-20 16:00:20 +0100

    name, nickname... in class contact

--- Cpp_Day_0/ex01/Contact.Classe.hpp
+++ Cpp_Day_0/ex01/Contact.Classe.hpp
@@ -6,7 +6,7 @@
 /*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:25:57 by bben-yaa          #+#    #+#             */
-/*   Updated: 2022/02/20 15:09:17 by bben-yaa         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:55:16 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,12 @@ class Contact{
 public:
 
 
-
+	std::string		first_name;
+	std::string		name;
+	std::string		last_name;
+	std::string		nickname;
+	std::string		phone_number;
+	std::string		darkest_secret;
 	Contact(void);
 	~Contact(void);
 };
--- Cpp_Day_0/ex01/main.cpp
+++ Cpp_Day_0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/20 14:32:50 by bben-yaa          #+#    #+#             */
-/*   Updated: 2022/02/20 15:28:12 by bben-yaa         ###   ########.fr       */
+/*   Updated: 2022/02/20 15:52:15 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 #include "Contact.Classe.hpp"
 #include "PhoneBook.Classe.hpp"
 
+void	add_contact(PhoneBook repo, char cmd[120])
+{
+	std::cout << "You gonna add a new conctat ->";
+	std::cout << " Enter the first name: ";
+	std::cin >> cmd;
+	repo.contact[0].name = cmd;
+	std::cout << "the name of the first contact is " << repo.contact[0].name << std::endl;
+}
+
 int	main(int argc, char **argv)
 {
 	PhoneBook	repo;
-	char		cmd[10];
+	char		cmd[120];
 
 	(void)argv;
 	if (argc == 1)
@@ -31,7 +40,7 @@ int	main(int argc, char **argv)
 			//std::cout << "You enter " << cmd << std::endl;
 			if (strcmp(cmd, "ADD") == 0)
 			{
-				std::cout << "you gonna add a new conctat" << std::endl;
+				add_contact(repo, cmd);
 			}
 			if (strcmp(cmd, "SEARCH") == 0)
 			{
