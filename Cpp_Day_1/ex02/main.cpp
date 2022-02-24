050361ae66d185212b215a246a7662797e31f737
balkisous <balkisous@gmail.com>
2022-02-24 08:54:43 +0100

    ex02 fini

--- Cpp_Day_1/ex02/main.cpp
+++ Cpp_Day_1/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: balkis <balkis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 23:15:27 by balkis            #+#    #+#             */
-/*   Updated: 2022/02/23 23:45:30 by balkis           ###   ########.fr       */
+/*   Updated: 2022/02/24 08:53:57 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,37 @@
 
 int	main(void)
 {
-	std::string	s_begin;
-	std::string	*stringPTR;
+	std::string	s_begin = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &s_begin;
 	std::string &stringREF = s_begin;
 
-	std::cout << "L'adresse de la string en mémoire est " << s_begin << std::endl;
-	std::cout << "L'adresse stockée dans la string est " << stringPTR << std::endl;
-	std::cout << "L’adresse stockée dans stringREF est " << stringREF << std::endl;
+	std::cout << "L'adresse de la string en mémoire est	" << s_begin << std::endl;
+	std::cout << "L'adresse stockée dans la string est	" << stringPTR << std::endl;
+	std::cout << "L’adresse stockée dans stringREF est	" << stringREF << std::endl << std::endl;
 
-	std::cout << "La valeur de la string est " << s_begin << std::endl;
-	std::cout << "La valeur pointée par stringPTR est " << stringPTR << std::endl;
-	std::cout << "La valeur pointée par stringREF est " << stringREF << std::endl;
+
+	std::cout << "La valeur de la string est		" << s_begin << std::endl;
+	std::cout << "La valeur pointée par stringPTR est	" << *stringPTR << std::endl;
+	std::cout << "La valeur pointée par stringREF est	" << stringREF << std::endl << std::endl;
+
+		*stringPTR = "No more brain T-T by ptr";
+
+	std::cout << "La valeur de la string est		" << s_begin << std::endl;
+	std::cout << "La valeur pointée par stringPTR est	" << *stringPTR << std::endl;
+	std::cout << "La valeur pointée par stringREF est	" << stringREF << std::endl << std::endl;
+
+
+		stringREF = "No more brain T-T by ref";
+
+
+	std::cout << "La valeur de la string est		" << s_begin << std::endl;
+	std::cout << "La valeur pointée par stringPTR est	" << *stringPTR << std::endl;
+	std::cout << "La valeur pointée par stringREF est	" << stringREF << std::endl << std::endl;
+
+	////Ce day a pour objectif de nous apprendre les references (nouvelle notion) et qu'elle
+	//// se comporte comme un pointeur toujours deferencer et qui doit obligatoirement pointee 
+	//// sur une quelque chose des qu'on la cree, elle pointe TOUJOURS sur une valeur
+	//// la valeur peut etre modifie comme le font les pointeurs
 
 	return (0);
 
