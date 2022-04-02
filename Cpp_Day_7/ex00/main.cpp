810397973c8a8f1bbfc71842201b61f2d63c9c65
balkisous <balkisous@gmail.com>
2022-04-02 12:38:54 +0200

    ex1 fini'

--- Cpp_Day_7/ex00/main.cpp
+++ Cpp_Day_7/ex00/main.cpp
@@ -3,16 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: balkis <balkis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 20:17:36 by balkis            #+#    #+#             */
-/*   Updated: 2022/04/01 20:18:31 by balkis           ###   ########.fr       */
+/*   Updated: 2022/04/02 12:38:28 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Whatever.hpp"
+#include "whatever.hpp"
+
+/*
+class Awesome
+{
+	public:
+		Awesome(void): _n(0) {}
+		Awesome(int n): _n(n) {}
+		Awesome &operator=(Awesome &a) {_n = a._n; return *this;}
+		bool operator==(Awesome const &rhs) const { return (this->_n == rhs._n);}
+		bool operator!=(Awesome const &rhs) const { return (this->_n != rhs._n);}
+		bool operator>(Awesome const &rhs) const { return (this->_n > rhs._n);}
+		bool operator<(Awesome const &rhs) const { return (this->_n < rhs._n);}
+		bool operator>=(Awesome const &rhs) const { return (this->_n >= rhs._n);}
+		bool operator<=(Awesome const &rhs) const { return (this->_n <= rhs._n);}
+		int get_n() const { return _n;}
+	private:
+		int _n;
+};
+
+std::ostream &operator<<(std::ostream &o, const Awesome &a) { o << a.get_n();return o;}
 
 int main(void)
 {
+	Awesome a(2), b(4);
+	std::cout << "............SWAP............" << std::endl;
+	std::cout << "Before swap function a = " << a << " and b = " << b << std::endl;
+	swap(a, b);
+	std::cout << "		" << a << " " << b << std::endl;
+	std::cout << "After swap function a = " << a << " and b = " << b << std::endl << std::endl;
+	
+	std::cout << "............MAX............" << std::endl;
+	std::cout << "Le max entre a: 4 et b: 2 est cencé etre a le resutat est ";
+	std::cout << max(a, b) << std::endl << std::endl;
+
+	std::cout << "............MIN............" << std::endl;
+	std::cout << "Le min entre a: 4 et b: 2 est cencé etre b le resutat est ";
+	std::cout << min(a, b) << std::endl << std::endl;
 	return (0);
+}
+*/
+
+int	main(void)
+{
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
--- Cpp_Day_7/ex00/whatever.hpp
+++ Cpp_Day_7/ex00/whatever.hpp
@@ -3,16 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: balkis <balkis@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bben-yaa <bben-yaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/01 20:18:38 by balkis            #+#    #+#             */
-/*   Updated: 2022/04/01 20:20:00 by balkis           ###   ########.fr       */
+/*   Updated: 2022/04/02 12:31:42 by bben-yaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
 
+
+
+#include <iostream>
+
+template <typename T>
+void	swap(T & a, T & b)
+{
+	T temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T &	min(T & a, T & b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template <typename T>
+T &	max(T & a, T & b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
 
 
 # endif
